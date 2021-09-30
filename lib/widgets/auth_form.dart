a1ae4ebaf6eda0b035f7add19d81e6cfa696fcc3
Nikhil Karve <karvenikhil26@gmail.com>
2021-09-29 21:36:22 -0400

    added .plist file

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fanpage_app/provider/gogle_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
